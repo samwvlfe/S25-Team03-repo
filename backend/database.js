704d4c445d98a98d5f3c68ee544c3a56597684b0
samwvlfe <r.sam.wolfe@gmail.com>
2025-04-17 10:37:56 -0400

    revert changes

--- backend/database.js
+++ backend/database.js
@@ -9,13 +9,7 @@ const port = process.env.PORT || 2999;
 
 // Middleware
 app.use(express.json());
-// app.use(cors());
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 //import hash func
 import {createHashPassword} from "../script/extra.js"
